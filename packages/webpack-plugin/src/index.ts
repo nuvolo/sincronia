8966942484bef229128914cb480241eefbdf0c4d
Breyton Barber <breyton.barber@0201-USA.local>
2019-08-23 16:22:22 -0600

    Add ability to load local webpack.config.js

--- packages/webpack-plugin/src/index.ts
+++ packages/webpack-plugin/src/index.ts
@@ -1,6 +1,7 @@
 import { Sinc } from "@sincronia/types";
 import memoryFS from "memory-fs";
 import webpack from "webpack";
+import path from "path";
 interface webpackPluginOpts {
   configGenerator?: (context: Sinc.FileContext) => webpack.Configuration;
   webpackConfig?: webpack.Configuration;
@@ -12,12 +13,20 @@ const run: Sinc.PluginFunc = async function(
 ): Promise<Sinc.PluginResults> {
   const memFS = new memoryFS();
   let wpOptions: webpack.Configuration = {};
+  let configFile = await loadWebpackConfig();
+  //First, try to load configuration file
+  if (configFile) {
+    Object.assign(wpOptions, configFile);
+  }
+  //Second, load from the options
   if (options.webpackConfig) {
     Object.assign(wpOptions, options.webpackConfig);
   }
+  //Third, load from configGenerator function
   if (options.configGenerator) {
     wpOptions = Object.assign(wpOptions, options.configGenerator(context));
   }
+  //override necesary parameters
   wpOptions.entry = context.filePath;
   wpOptions.output = {
     path: "/",
@@ -31,19 +40,32 @@ const run: Sinc.PluginFunc = async function(
         reject(err);
         return;
       }
-      //console.log(test);
       resolve(memFS.readFileSync("/bundle.js", "utf-8"));
     });
   });
   try {
     let output = await compilePromise;
-    //console.log(output);
     return {
       output,
       success: true
     };
   } catch (e) {
     throw new Error();
+  }
+  function getWebpackConfigPath() {
+    let pathChunks = context.filePath.split(path.sep);
+    pathChunks.pop();
+    pathChunks.push("webpack.config.js");
+    return path.sep + path.join(...pathChunks);
+  }
+  async function loadWebpackConfig() {
+    try {
+      let configPath = getWebpackConfigPath();
+      let config: webpack.Configuration = (await import(configPath)).default;
+      return config;
+    } catch (e) {
+      return false;
+    }
   }
 };
 
