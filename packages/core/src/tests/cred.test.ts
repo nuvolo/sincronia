f1b235f5ee25f3be27fe67a290df62d9c1f659dd
Tyler Edwards <tredwards2002@gmail.com>
2020-06-23 18:28:20 -0400

    Creates Env file at correct spot

--- packages/core/src/tests/cred.test.ts
+++ packages/core/src/tests/cred.test.ts
@@ -1,4 +1,5 @@
 import fs from "fs"
+import path from "path";
 import {init} from "../bootstrap"
 import dotenv from "dotenv";
 import ConfigManager from "../config";
@@ -6,26 +7,30 @@ import ConfigManager from "../config";
 
 ConfigManager.getEnvPath = jest.fn(() => ".env")
 
-
-
+let envPath = path.join(process.cwd(), ".env");
+console.log(envPath)
 describe('Test ENVCredentials', () =>{
-    it("Credentials should be Undefined when False", () => {
+    it("Credentials should be Undefined when file is false", () => {
         init()
 
         expect(process.env.SN_USER).toBeUndefined()
         expect(process.env.SN_PASSWORD).toBeUndefined()
         expect(process.env.SN_INSTANCE).toBeUndefined()
     })
+    it("Credentials should be real when file is real", () => {
+        fs.writeFile(envPath, 
+            'SN_USER=New \nSN_PASSWORD=Edwards \nSN_INSTANCE=dev90755.service-now.com', 
+            (err) =>{
+                if (err) throw err;
+            });
+        init()
+        
+        expect(process.env.SN_USER).toBeUndefined()
+        expect(process.env.SN_PASSWORD).toBeUndefined()
+        expect(process.env.SN_INSTANCE).toBeUndefined()
+    })
     
-     fs.writeFile(".env", 
-    'SN_USER=Tyler \nSN_PASSWORD=Edwards \nSN_INSTANCE=dev90755.service-now.com', 
-    (err) =>{
-        if (err) throw err;
-    });
     
-})
-
-describe('Test ENVCredentials', () =>{
     
     
 })
