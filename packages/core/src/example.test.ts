8cbf16eda5fe6a88265d1ff17f36fe40627ab0b2
collin parker <collin.parker@nuvolo.com>
2020-07-06 10:24:28 -0700

    removed extra test

--- packages/core/src/example.test.ts
+++
@@ -1,8 +0,0 @@
-describe('Sample Test', () => {
-    it('should test that true === true', () => {
-      expect(true).toBe(true);
-    });
-    it('should test that true === false', () => {
-        expect(true).toBe(false);
-      });
-  });
